1ff37906c3e129a4f282c382a890acd553c72b94
Andreas Thalhammer <athalhammer@users.noreply.github.com>
2017-06-22 22:27:50 +0200

    included tracking of dump date

--- createLinks.sh
+++ createLinks.sh
@@ -6,13 +6,15 @@ pagelinks="$1""wiki-latest-pagelinks.sql"
 pageprops="$1""wiki-latest-page_props.sql"
 page="$1""wiki-latest-page.sql"
 redirects="$1""wiki-latest-redirect.sql"
+md5sums="$1""wiki-latest-md5sums.txt"
 
 ###################### DOWNLOAD AND UNZIP
-wget -q "$url""$pagelinks"".gz" "$url""$pageprops"".gz" "$url""$page"".gz" "$url""$redirects"".gz"
+wget -q "$url""$pagelinks"".gz" "$url""$pageprops"".gz" "$url""$page"".gz" "$url""$redirects"".gz" "$url""$md5sums"
 gunzip -f "$1"*.gz
 
 ###################### PRE-PROCESSING
 export LC_ALL=C.UTF-8
+dump_date=`head -n 1 "$1"wiki-latest-md5sums.txt | sed "s/\(.*\) \(.*\)-\(.*\)-\(.*\)/\3/"`
 python3 lib/preprocess.py $pagelinks | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)',0)\(.*\)/\2\t\3/p"  > "$1""pagelinks.lines"
 python3 lib/preprocess.py $pageprops | sed -n "s/\(.*\)(\(.*\),'wikibase_item','\(.*\)',\(.*\))\(.*\)/\3\t\2/p" > "$1""pageprops.lines"
 python3 lib/preprocess.py $redirects | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)','\(.*\)','\(.*\)')\(.*\)/\2\t\3/p" > "$1""redirects.lines"
@@ -24,7 +26,7 @@ else
 python3 lib/preprocess.py $page | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)'\(,.*\)\{11\})\(.*\)/\2\t\3/p" > "$1""page.lines"
 fi
 rm "$1"*.sql
-
+rm "$1"*.txt
 ###################### JOINS
 export LC_ALL=C
 sort --field-separator=$'\t' --key=2 -o "$1""page.lines" "$1""page.lines"
@@ -45,6 +47,6 @@ sort --field-separator=$'\t' --key=2 -o "$1""pageprops.lines" "$1""pageprops.lin
 python3 lib/join.py "$1""pagelinks2.lines" "$1""pageprops.lines" | sed "s/\(.*\)\t\(.*\)/\2\t\1/" > "$1""pagelinks.lines"
 sort --field-separator=$'\t' --key=2 -o "$1""pagelinks.lines" "$1""pagelinks.lines"
 python3 lib/join.py "$1""pagelinks.lines" "$1""pageprops.lines" | sed "s/Q\(.*\)\tQ\(.*\)/\2\t\1/" > "$1""pagelinks2.lines"
-sort -n "$1""pagelinks2.lines" | uniq > "$1"".links"
-bzip2 "$1"".links"
+sort -n "$1""pagelinks2.lines" | uniq > "$1"-"$dump_date"".links"
+#bzip2 "$1"".links"
 rm "$1"*.lines
