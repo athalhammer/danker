b15d11461ae5c381201a99c501ce74602835b505
athalhammer <andreas@thalhammer.bayern>
2017-06-30 16:12:40 +0000

    code cleaning

--- danker.sh
+++ danker.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mkdir results
-#./getLanguages.sh
-for i in `cat wiki.langs`; do ./createLinks.sh "$i" "results"; done
-for i in `ls ./results/*.links`; do sort --field-separator=$'\t' --key=2 -o "$i"".right" "$i"; done
-for i in `ls ./results/*.links`; do python3 ./lib/danker.py  "$i" "$i"".right"  0.85 40 0.1 > "$i".rank; done
-for i in `ls ./results/*.right`; do rm "$i"; done
-for i in `ls ./results/*.rank`; do sort -nro "$i" --field-separator=$'\t' --key=2 "$i"; done
+folder="results"
+mkdir "$folder"
+
+#./lib/getLanguages.sh
+for i in `cat wiki.langs`; do ./lib/createLinks.sh "$i" "$folder"; done
+for i in `ls ./$folder/*.links`; do sort --field-separator=$'\t' --key=2 -o "$i"".right" "$i"; done
+for i in `ls ./$folder/*.links`; do python3 ./lib/danker.py  "$i" "$i"".right"  0.85 40 0.1 > "$i".rank; done
+for i in `ls ./$folder/*.right`; do rm "$i"; done
+for i in `ls ./$folder/*.rank`; do sort -nro "$i" --field-separator=$'\t' --key=2 "$i"; done
 
--- getLanguages.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -s http://wikistats.wmflabs.org/display.php?t=wp | sed -n "s/        <td class=\"text\"><a href=\(.*\)>\(.*\)<\/a><\/td><td class=\"text\"><a href=\(.*\)>\(.*\)<\/a><\/td>/\4/p" | sort > wiki.langs
