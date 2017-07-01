9843068e1a3532792caf7e7149ace0bd9bff47da
athalhammer <andreas@thalhammer.bayern>
2017-07-01 17:18:17 +0000

    changed danker setup

--- danker.sh
+++ danker.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-folder="results"
-mkdir "$folder"
-
-#./lib/getLanguages.sh
-for i in `cat wiki.langs`; do ./lib/createLinks.sh "$i" "$folder"; done
-for i in `ls ./$folder/*.links`; do sort --field-separator=$'\t' --key=2 -o "$i"".right" "$i"; done
-for i in `ls ./$folder/*.links`; do python3 ./lib/danker.py  "$i" "$i"".right"  0.85 40 0.1 > "$i".rank; done
-for i in `ls ./$folder/*.right`; do rm "$i"; done
-for i in `ls ./$folder/*.rank`; do sort -nro "$i" --field-separator=$'\t' --key=2 "$i"; done
-
+filename=`./lib/createLinks.sh "$1"`
+sort --field-separator=$'\t' --key=2 -o "$filename"".right" "$filename"
+python3 ./lib/danker.py  "$filename" "$filename"".right"  0.85 40 0.1 > "$filename".rank
+sort -nro "$filename"".rank" --field-separator=$'\t' --key=2 "$filename"".rank"
+rm "$filename"".right"
