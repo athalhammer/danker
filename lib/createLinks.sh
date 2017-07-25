4c5e039d6737de2eabc1f4d701b68c64374d91de
athalhammer <andreas@thalhammer.bayern>
2017-07-25 21:01:34 +0000

    replaced preprocess.py with sed command

--- lib/createLinks.sh
+++ lib/createLinks.sh
@@ -30,15 +30,15 @@ gunzip -f "$1"*.gz
 ###################### PRE-PROCESSING
 export LC_ALL=C.UTF-8
 dump_date=`head -n 1 "$1"wiki-latest-md5sums.txt | sed "s/\(.*\) \(.*\)-\(.*\)-\(.*\)/\3/"`
-python3 ./lib/preprocess.py $pagelinks | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)',0)\(.*\)/\2\t\3/p"  > "$1""pagelinks.lines"
-python3 ./lib/preprocess.py $pageprops | sed -n "s/\(.*\)(\(.*\),'wikibase_item','\(.*\)',\(.*\))\(.*\)/\3\t\2/p" > "$1""pageprops.lines"
-python3 ./lib/preprocess.py $redirects | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)','\(.*\)','\(.*\)')\(.*\)/\2\t\3/p" > "$1""redirects.lines"
+sed -n "s/),(/)\n(/gp" $pagelinks | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)',0)\(.*\)/\2\t\3/p" > "$1""pagelinks.lines"
+sed -n "s/),(/)\n(/gp" $pageprops | sed -n "s/\(.*\)(\(.*\),'wikibase_item','\(.*\)',\(.*\))\(.*\)/\3\t\2/p" > "$1""pageprops.lines"
+sed -n "s/),(/)\n(/gp" $redirects | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)','\(.*\)','\(.*\)')\(.*\)/\2\t\3/p" > "$1""redirects.lines"
 
 # the page table does not always contain 14 fields (has 14 in MediaWiki 1.30.0-wmf.1, additional column "page_no_title_convert" in MediaWiki 1.30.0-wmf.2)
 if grep -q 'page_no_title_convert' $page; then
-python3 ./lib/preprocess.py $page | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)'\(,.*\)\{12\})\(.*\)/\2\t\3/p" > "$1""page.lines"
+sed -n "s/),(/)\n(/gp" $page | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)'\(,.*\)\{12\})\(.*\)/\2\t\3/p" > "$1""page.lines"
 else
-python3 ./lib/preprocess.py $page | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)'\(,.*\)\{11\})\(.*\)/\2\t\3/p" > "$1""page.lines"
+sed -n "s/),(/)\n(/gp" $page | sed -n "s/\(.*\)(\(.*\),0,'\(.*\)'\(,.*\)\{11\})\(.*\)/\2\t\3/p" > "$1""page.lines"
 fi
 rm "$1"*.sql
 rm "$1"*.txt
