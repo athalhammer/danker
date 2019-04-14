745cc2c00ec95cac1dbec05af463317caef0c8cf
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-04-14 21:14:20 +0200

    added comments for test cases, cleaned up code

--- test/danker_test.py
+++ test/danker_test.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
+"""
+danker test cases
+"""
 import unittest
 import networkx as nx
 import numpy as np
 import lib.danker
 
 class DankerTest(unittest.TestCase):
+    """
+    danker test cases
+    """
 
     def _compare(self, netx, danker):
+        """
+        Helper method to compare danker and NetworkX outputs.
+        """
         self.assertEqual(len(netx), len(danker))
         self.assertEqual(len(netx.keys() - danker.keys()), 0)
         self.assertEqual(len(danker.keys() - netx.keys()), 0)
@@ -21,17 +30,26 @@ class DankerTest(unittest.TestCase):
         self.assertAlmostEqual(np.corrcoef(res[0], res[1])[0][1], 1, places=6)
 
     def test_assert_left_sort(self):
+        """
+        Test if assert for left sort works (test with right sorted file)
+        """
         link_file_right = "./test/graphs/test.links.right"
         with self.assertRaises(AssertionError):
             lib.danker.init(link_file_right, 0.1, False)
 
     def test_assert_right_sort(self):
+        """
+        Test if assert for right sort works (test with left sorted file)
+        """
         link_file = "./test/graphs/test.links"
         danker_graph = lib.danker.init(link_file, 0.1, True)
         with self.assertRaises(AssertionError):
-            danker_pr_small = lib.danker.danker_smallmem(danker_graph, link_file, 50, 0.85, 0.1)
+            lib.danker.danker_smallmem(danker_graph, link_file, 50, 0.85, 0.1)
 
     def test_general(self):
+        """
+        Test with a very small graph and comparision between danker and NetworkX.
+        """
         link_file = "./test/graphs/test.links"
         link_file_right = "./test/graphs/test.links.right"
         nx_graph = nx.read_edgelist(link_file, create_using=nx.DiGraph, nodetype=str,
@@ -44,6 +62,9 @@ class DankerTest(unittest.TestCase):
         self._compare(nx_pr, danker_pr_small)
 
     def test_bar(self):
+        """
+        Test with a small graph (Bavarian Wikilinks) and comparision between danker and NetworkX.
+        """
         link_file = "./test/graphs/bar-20190301.links"
         link_file_right = "./test/graphs/bar-20190301.links.right"
         nx_graph = nx.read_edgelist(link_file, create_using=nx.DiGraph, nodetype=int,
