5803d9c4a9c79ffd68a656b97fdc54051da3026a
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-05-11 16:37:41 +0200

    updated getLanguages script to handle wikinames

--- script/getLanguages.sh
+++ script/getLanguages.sh
@@ -16,4 +16,8 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-curl -s "http://wikistats.wmflabs.org/display.php?t=wp" | sed -n 's;\(.*text\)\{2\}.*>\(.*\)</a>.*;\2;p' | sort
+curl -s "http://wikistats.wmflabs.org/display.php?t=wp" \
+	| sed -n 's;\(.*text\)\{2\}.*>\(.*\)</a>.*;\2;p'  `# Parse wiki names` \
+	| sed "s/-/_/g"                                   `# Replace "-" with "_"` \
+	| sed "s/be_tarask/be_x_old/" 	                  `# Manual fix for be_x_old` \
+	| sort
