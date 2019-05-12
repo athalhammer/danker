dd9aafbf82ab58c5b2e0958f8d42a2cc08592e24
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-05-12 23:28:07 +0200

    removed second dictionary

--- danker/danker.py
+++ danker/danker.py
@@ -39,14 +39,14 @@ def _get_std_tuple(smallmem, start_value):
     Helper function to return standard tuple (with or without linked list)
     """
     if smallmem:
-        return (0, start_value)
-    return (0, start_value, [])
+        return [0, start_value, start_value, False]
+    return [0, start_value, start_value, []]
 
 def init(left_sorted, start_value, smallmem):
     """
     Read left_sorted link file and initialization steps.
     """
-    dictionary_i_1 = {}
+    dictionary = {}
     previous = -1
     current_count = 1
     with open(left_sorted, encoding="utf-8") as ls_file:
@@ -56,10 +56,8 @@ def init(left_sorted, start_value, smallmem):
 
             # take care of inlinks
             if not smallmem:
-                data = dictionary_i_1.get(receiver, _get_std_tuple(smallmem, start_value))
-                data[2].append(current)
-                # make sure to store it in the dict (could be there already)
-                dictionary_i_1[receiver] = data
+                data = dictionary.setdefault(receiver, _get_std_tuple(smallmem, start_value))
+                data[3].append(current)
 
             # take care of counts
             if current == previous:
@@ -70,67 +68,77 @@ def init(left_sorted, start_value, smallmem):
                     # make sure input is correctly sorted
                     assert current > previous, _INPUT_ASSERTION_ERROR.format(left_sorted,
                                                                              current, previous)
-
-                    # store previousQID and reset counter
-                    prev = dictionary_i_1.get(previous, _get_std_tuple(smallmem, start_value))
-                    dictionary_i_1[previous] = (current_count,) + prev[1:]
+                    # store previous Q-ID and reset counter
+                    prev = dictionary.get(previous, _get_std_tuple(smallmem, start_value))
+                    dictionary[previous] = [current_count] + prev[1:]
                     current_count = 1
             previous = current
 
         # take care of last item
         if previous != -1:
-            prev = dictionary_i_1.get(previous, _get_std_tuple(smallmem, start_value))
-            dictionary_i_1[previous] = (current_count,) + prev[1:]
-    return dictionary_i_1
+            prev = dictionary.get(previous, _get_std_tuple(smallmem, start_value))
+            dictionary[previous] = [current_count] + prev[1:]
+    return dictionary
 
-def danker_smallmem(dictionary_i_1, right_sorted, iterations, damping, start_value):
+def danker_smallmem(dictionary, right_sorted, iterations, damping, start_value):
     """
     Compute PageRank with right sorted file.
     """
-    dictionary_i = {}
-    for i in range(0, iterations):
-        print(str(i + 1) + ".", end="", flush=True, file=sys.stderr)
+    for iteration in range(0, iterations):
+        print(str(iteration + 1) + ".", end="", flush=True, file=sys.stderr)
         previous = 0
+
+        # positions for i and i+1 result values (alternating with iterations).
+        i_location = (iteration % 2) + 1
+        i_plus_1_location = ((iteration + 1) % 2) + 1
+
+        # track nodes with inlinks
+        to_nodes = []
         with open(right_sorted, encoding="utf-8") as rs_file:
             for line in rs_file:
                 current = _conv_int(line.split("\t")[1].strip())
+                to_nodes.append(current)
                 if previous != current:
                     assert previous == 0 or current > previous, _INPUT_ASSERTION_ERROR.format(
                         right_sorted, current, previous)
+                    # reset dank
                     dank = 1 - damping
-                current_dank = dictionary_i_1.get(current, (0, start_value))
-                in_dank = dictionary_i_1.get(_conv_int(line.split("\t")[0].strip()))
-                dank = dank + (damping * in_dank[1] / in_dank[0])
-                dictionary_i[current] = current_dank[0], dank
+
+                current_dank = dictionary.setdefault(current, [0, start_value, start_value])
+                in_link = _conv_int(line.split("\t")[0].strip())
+                in_dank = dictionary.get(in_link)
+                dank = dank + (damping * in_dank[i_location] / in_dank[0])
+                dictionary[current][i_plus_1_location] = dank
                 previous = current
 
-        # after each iteration, fix nodes that don't have inlinks
-        for j in dictionary_i_1.keys() - dictionary_i.keys():
-            dictionary_i[j] = dictionary_i_1[j][0], 1 - damping
-        dictionary_i_1 = dictionary_i
-        dictionary_i = {}
-
+            # fix nodes where no update happened in the fist iteration
+            if iteration == 0:
+                for k in dictionary.keys() - to_nodes:
+                        dictionary[k][i_plus_1_location] = 1 - damping
+                        dictionary[k][i_location] = 1 - damping
     print("", file=sys.stderr)
-    return dictionary_i_1
+    return dictionary
 
-def danker_bigmem(dictionary_i_1, iterations, damping):
+def danker_bigmem(dictionary, iterations, damping):
     """
     Compute PageRank with big memory option.
     """
-    dictionary_i = {}
-    for i in range(0, iterations):
-        print(str(i + 1) + ".", end="", flush=True, file=sys.stderr)
-        for j in dictionary_i_1:
-            current = dictionary_i_1.get(j)
+    for iteration in range(0, iterations):
+
+        # positions for i and i+1 result values (alternating with iterations).
+        i_location = (iteration % 2) + 1
+        i_plus_1_location = ((iteration + 1) % 2) + 1
+
+        print(str(iteration + 1) + ".", end="", flush=True, file=sys.stderr)
+        for j in dictionary:
+            current = dictionary.get(j)
             dank = 1 - damping
-            for k in current[2]:
-                in_dank = dictionary_i_1.get(k)
-                dank = dank + (damping * in_dank[1] / in_dank[0])
-            dictionary_i[j] = current[0], dank, current[2]
-        dictionary_i_1 = dictionary_i
-        dictionary_i = {}
+            for k in current[3]:
+                in_dank = dictionary.get(k)
+                dank = dank + (damping * in_dank[i_location] / in_dank[0])
+            dictionary[j][i_plus_1_location] = dank
     print("", file=sys.stderr)
-    return dictionary_i_1
+    return dictionary
 
 #@profile
 def _main():
@@ -147,14 +155,15 @@ def _main():
     start = time.time()
     dictionary_i_1 = init(args.left_sorted, args.start_value, args.right_sorted)
     if args.right_sorted:
-        dictionary_i = danker_smallmem(dictionary_i_1, args.right_sorted,
+        danker_smallmem(dictionary_i_1, args.right_sorted,
                                        args.iterations, args.damping, args.start_value)
     else:
-        dictionary_i = danker_bigmem(dictionary_i_1, args.iterations, args.damping)
+        danker_bigmem(dictionary_i_1, args.iterations, args.damping)
     print("Computation of PageRank on '{0}' with {1} took {2:.2f} seconds.".format(
         args.left_sorted, 'danker', time.time() - start), file=sys.stderr)
-    for i in dictionary_i:
-        print("{0}\t{1:.17g}".format(i, dictionary_i[i][1]))
+
+    for i in dictionary_i_1:
+        print("{0}\t{1:.17g}".format(i, dictionary_i_1[i][((args.iterations) % 2) + 1]))
 
 if __name__ == '__main__':
     _main()
