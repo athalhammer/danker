70836aa9cdb89af829087769f8f6bf08c9880746
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-05-29 16:39:38 +0200

    v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='danker',
-    version='0.3.2',
+    version='0.4.0',
     url='https://github.com/athalhammer/danker',
     author='Andreas Thalhammer',
     author_email='andreas@thalhammer.bayern',
