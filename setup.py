6a78809cca8b5bdb9819b9fa71c89d5a4ba9057d
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-09-14 14:27:35 +0200

    updated setup.py [skip ci]

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
     name='danker',
@@ -6,7 +9,20 @@ setup(
     url='https://github.com/athalhammer/danker',
     author='Andreas Thalhammer',
     author_email='andreas@thalhammer.bayern',
-    description='Compute PageRank on ~3 billion Wikipedia links on off-the-shelf hardware.',
-    packages=find_packages(),
+    description='Compute PageRank on large graphs on ' +
+                'off-the-shelf hardware.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['danker'],
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
+    python_requires='>=3.4',
     install_requires=[]
 )
