6c3fbc136cf5569428f8702ef7dd2a20f463429b
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-10-06 12:10:03 +0200

    improved doc

--- danker.sh
+++ danker.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#    danker - PageRank on Wikipedia/Wikidata
+#    Copyright (C) 2019  Andreas Thalhammer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # reproduce all arguments for Python's argparse
 args=""
 while [[ $# -gt 0 ]]; do
@@ -7,11 +24,11 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
-# argparse with Python
+# Flexible argparse with Python and formatting for bash.
 formatted=$(python3 ./script/args.py $args)
 pyexit=$?
 
-# should not contain "usage" (e.g., from --help)
+# Output should not contain "usage" (e.g., from --help)
 echo "$formatted" | grep "usage" > /dev/null
 formexit=$?
 
--- script/args.py
+++ script/args.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+
+#    danker - PageRank on Wikipedia/Wikidata
+#    Copyright (C) 2019  Andreas Thalhammer
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 import argparse
 
 parser = argparse.ArgumentParser(prog='./danker.sh', 
@@ -17,6 +36,8 @@ parser.add_argument('-s', '--start', type=float, default=0.1,
 parser.add_argument('-b', '--bigmem', action='store_true',
                     help='Switch for "big memory" option.')
 args = parser.parse_args()
+
+# Preparing arguments for Bash
 if args.project:
     if args.project != 'wiki':
         print('-p', args.project, end='')
