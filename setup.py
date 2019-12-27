bf9f2242854f38286452cedcef76de1c51baa5cc
Andreas Thalhammer <andreas@thalhammer.bayern>
2019-12-27 10:06:52 +0100

    added python 3.8

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Information Analysis',
