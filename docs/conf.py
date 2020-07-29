3214254c031c9b94c835234b681ccbf64f71bec6
Andreas Thalhammer <andreas@thalhammer.bayern>
2020-07-29 16:43:44 +0200

    v0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Andreas Thalhammer'
 author = 'Andreas Thalhammer'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.6.0'
 
 # -- General configuration ---------------------------------------------------
 
