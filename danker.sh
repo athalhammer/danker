299717a3656bede94cf5068b2d2c5a96bdf8d89b
Andreas Thalhammer <andreas@thalhammer.bayern>
2021-11-03 21:39:38 +0100

    added set -e

--- danker.sh
+++ danker.sh
@@ -15,7 +15,7 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+set -e
 
 
 # Flexible argparse with Python and formatting for bash.
