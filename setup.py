fc308a084242b29ff49342d62062eddf2de85a39
Andreas Thalhammer <andreas@thalhammer.bayern>
2022-10-08 16:10:43 +0200

    release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README_PR.md", "r") as f:
 
 setup(
     name='danker',
-    version='0.7.2',
+    version='0.7.3',
     url='https://github.com/athalhammer/danker',
     author='Andreas Thalhammer',
     author_email='andreas@thalhammer.bayern',
@@ -15,14 +15,12 @@ setup(
     long_description_content_type="text/markdown",
     packages=['danker'],
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering :: Information Analysis',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[]
 )
