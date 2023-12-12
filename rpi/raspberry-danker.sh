62327d8115f81c19e09c4f624e95655f8aba6ec6
Andreas Thalhammer <andreas@thalhammer.bayern>
2023-12-12 23:12:53 +0100

    rpi processing script added

--- rpi/raspberry-danker.sh
+++ rpi/raspberry-danker.sh
@@ -15,15 +15,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Asumes AWS Cli and according settings 
+# Expects AWS Cli and according settings 
 export MEM_PERC="90%"
 export S3_BUCKET="danker"
+export INDEX_FILE="index.html"
+export PROJECT_LINKS=".allwiki.links"
 
 filename=$(./danker.sh ALL)
-VER=$(echo "$filename" | sed "s/.allwiki.links//")
-aws s3 cp s3://"$S3_BUCKET"/index.html .
+VER=$(echo "$filename" | sed "s/$PROJECT_LINKS//")
+aws s3 cp s3://"$S3_BUCKET/$INDEX_FILE" .
 cat ./rpi/template | sed "s/VERSION/$VER/" > tmp
-perl -i -p0e 's/  "distribution":\[/`cat tmp`/se' index.html
-aws s3 cp index.html s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-aws s3 cp "$VER.allwiki.links.rank.bz2" s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-aws s3 cp "$VER.allwiki.links.stats.txt" s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+perl -i -p0e 's/  "distribution":\[/`cat tmp`/se' "$INDEX_FILE"
+aws s3 cp "$INDEX_FILE" s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp "$filename.rank.bz2" s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp "$filename.stats.txt" s3://"$S3_BUCKET"/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
